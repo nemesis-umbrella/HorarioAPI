33b2eba97d2dafaeff2c09f3a9e01091d34b9945
Jorge L. Mondragon <nemesis_umbrella@outlook.com>
2020-07-17 22:59:19 -0500

    Cambios temporales para profesor

--- asistencia/urls.py
+++ asistencia/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import perfil, listar_horario, listar_asistencia
+from .views import perfil, listar_horario, listar_asistencia, listar_clases_profesor
 
 urlpatterns = [
-    path('alumno/', perfil, name = 'perfil'),
+    path('alumno/', perfil, name = 'perfil_alumno'),
     path('alumno/horario/', listar_horario, name = 'horario_list'),
     path('alumno/asistencia/', listar_asistencia, name = 'asistencia_list'),
+    path('profesor/', perfil, name = 'perfil_profesor'),
+    path('profesor/clases', listar_clases_profesor, name = 'clases_profesor'),
 ]
--- asistencia/views.py
+++ asistencia/views.py
@@ -85,3 +85,30 @@ def listar_asistencia(request):
             raise Http404("No existe la consulta")
     else:
         raise Http404("No existe la operación")
+
+@login_required
+def listar_clases_profesor(request):
+    if request.method == 'GET':
+        user_id = request.user.id
+        user = get_object_or_404(User, pk=user_id)
+        if user.has_perm('api.es_profesor'):
+            profesor = get_object_or_404(Profesor,clave_empleado=user.get_username())
+            clase_horarios = ClaseHorario.objects.filter(profesor=profesor,estado=1)
+            contador = 1
+            for clase_horario in clase_horarios:
+                alumno_horarios = AlumnoHorario.objects.filter(clase_horario=clase_horario)
+                asistencias = Asistencia.objects.filter(alumno_horario__in=alumno_horarios).exclude(fecha__isnull=True).values_list('fecha',flat = True).distinct()
+                dict_clase = {
+                    'no' : contador,
+                    'materia' : clase_horario.materia,
+                    'lunes' : clase_horario.lunes,
+                    'martes' : clase_horario.martes,
+                    'miercoles' : clase_horario.miercoles,
+                    'jueves' : clase_horario.jueves,
+                    'viernes' : clase_horario.viernes,
+                    'sabado' : clase_horario.sabado,
+                    'total_asist': len(asistencias),
+                }
+                print(dict_clase)
+                contador += 1
+            raise Http404("No existe la consulta")
