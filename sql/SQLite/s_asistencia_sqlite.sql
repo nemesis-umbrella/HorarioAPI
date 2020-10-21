f2638cd7f1b969ac2583807b33b811f4c04f9c90
Jorge L. Mondragón <36802843+nemesis-umbrella@users.noreply.github.com>
2020-10-20 20:04:13 -0500

    Update s_asistencia_sqlite.sql
    Ocultar algunos datos

--- sql/SQLite/s_asistencia_sqlite.sql
+++ sql/SQLite/s_asistencia_sqlite.sql
@@ -135,7 +135,7 @@ INSERT INTO dt_profesor VALUES(6789512,'Jill', 'Valentine',1);
 INSERT INTO dt_profesor VALUES(8964481,'Chris', 'Redfield',1);
 INSERT INTO dt_profesor VALUES(5964412,'Albert', 'Wesker',1);
 INSERT INTO dt_profesor VALUES(1291564,'Claudia', 'Morán',1);
-INSERT INTO dt_profesor VALUES(2015544,'José Luis', 'Limón',1);
+INSERT INTO dt_profesor VALUES(2015544,'José', 'Limón',1);
 
 --
 -- Volcado de datos para la tabla 'dt_alumno'
@@ -143,5 +143,5 @@ INSERT INTO dt_profesor VALUES(2015544,'José Luis', 'Limón',1);
 INSERT INTO dt_alumno VALUES (201450484,'Jorge L.', 'Mondragón',1,'INFO');
 INSERT INTO dt_alumno VALUES (201459920,'Daniel', 'Peréz',1,'SIST');
 INSERT INTO dt_alumno VALUES (201455699,'Alexa', 'Reyes',1,'BQUIM');
-INSERT INTO dt_alumno VALUES (201446462,'Nancy', 'Zavala',1,'GESTE');
-INSERT INTO dt_alumno VALUES (201445897,'Juan', 'Sanchéz',1,'AERO');
+INSERT INTO dt_alumno VALUES (201446462,'Nancy', 'Zepeda',1,'GESTE');
+INSERT INTO dt_alumno VALUES (201445897,'Juan', 'Sanachez',1,'AERO');
